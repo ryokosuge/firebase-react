57f9c3480b374a221aff7adfeda505d83458b468
ryokosuge <ryo.kosuge@gmail.com>
2021-06-01 11:55:56 +0900

    chore:	公開

---
+++ src/firebase-params.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC25SbLtZD1D-Zr6-hnTQqE3I5pFmPVskE",
+    authDomain: "authentication-sample-2eb0a.firebaseapp.com",
+    projectId: "authentication-sample-2eb0a",
+    storageBucket: "authentication-sample-2eb0a.appspot.com",
+    messagingSenderId: "175862164527",
+    appId: "1:175862164527:web:4877ccc7061519664d2ada",
+    measurementId: "G-JKW66DT228"
+};
