83ef320ee62780b3f4ee998602fbcbe1067a9cf7
ryokosuge <ryo.kosuge@gmail.com>
2021-06-01 11:47:57 +0900

    feat:	auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase"
+import { firebaseConfig } from "./firebase-params";
+
+firebase.initializeApp(firebaseConfig)
 
 function App() {
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [user, setUser] = React.useState<firebase.User | null>(null);
+  const [idToken, setIdToken] = React.useState<string>("");
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    firebase.auth().onAuthStateChanged(async (user: firebase.User | null) => {
+      if (user) {
+        // 引数はforceRefresh
+        const idToken = await user.getIdToken(true)
+        setIdToken(idToken)
+      }
+      console.log(user);
+      setUser(user);
+      setIsLoading(false);
+    })
+  }, [setUser])
+
+  const handleClick = React.useCallback(() => {
+    const login = async () => {
+      setIsLoading(true);
+      const provider = new firebase.auth.GoogleAuthProvider()
+      await firebase.auth().signInWithRedirect(provider);
+      setIsLoading(false);
+    }
+    login();
+  }, [setIsLoading]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : user !== null ? (
+          <dl>
+            <dt>user.email</dt>
+            <dd>{user.email}</dd>
+            <dt>idToke</dt>
+            <dd>{idToken}</dd>
+          </dl>
+        ) : (
+          <button onClick={handleClick}>ログイン</button>
+        )}
       </header>
     </div>
   );
